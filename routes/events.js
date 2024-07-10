e675e00dee214fb5003c9152f10c1cbe265e89b2
Sebastián Mayor <naitsabes29@gmail.com>
2024-07-10 00:35:44 -0500

    Added edit event functionality
    Minor fix
    
    Minor fix

--- routes/events.js
+++ routes/events.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const eventController = require("../controllers/events");
 
 router.post("/", eventController.createEvent);
+router.put("/:id", eventController.editEvent )
 router.get("/list", eventController.getEventsByBusinessId);
 
 module.exports = router;
